60b5c2046ec4e871672d33672f7627ee9ac1e39e
sajith <sajithgithub@gmail.com>
2020-11-27 09:42:16 +0530

    Improved

--- src/image_color_selection.py
+++ src/image_color_selection.py
@@ -6,3 +6,24 @@ image = mpimg.imread("../images/test.jpg")
 
 print("This image is: ", type(image))
 print("Image shape", image.shape)
+
+#
+ysize = image.shape[0]
+xsize = image.shape[1]
+
+color_select = np.copy(image)
+
+red_threshold = 0
+green_threshold = 0
+blue_threashold = 0
+
+rgb_threashold = [red_threshold, green_threshold, blue_threashold]
+
+thresholds = (image[:,:,0] < rgb_threashold[0]) \
+            |(image[:,:,1] < rgb_threashold[1])\
+            |(image[:,:,2] < rgb_threashold[2])
+
+color_select[thresholds] = [0,0,0]
+
+plt.imshow(color_select)
+plt.show()
