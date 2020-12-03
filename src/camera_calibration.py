0416b94030c71f1da2750dce41fcc180d0775cac
sajith <sajith@digitalxlabs.com>
2020-12-03 15:53:50 +0530

    Added a sample for camera calibration

--- src/camera_calibration.py
+++ src/camera_calibration.py
@@ -1,7 +1,10 @@
 import numpy as np
 import cv2
 import glob
+import matplotlib
+matplotlib.use('agg')
 import matplotlib.pyplot as plt
+import pickle
 
 # prepare objects points
 objp = np.zeros((6*8, 3), np.float32)
@@ -34,4 +37,23 @@ for inx, fname in enumerate(images):
 
 cv2.destroyAllWindows()
 
+img = cv2.imread('../calibration_wide/test_image.jpg')
+img_size = (img.shape[1], img.shape[0])
 
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints,imgpoints,img_size, None, None)
+
+
+dst = cv2.undistort(img,mtx,dist,None,mtx)
+cv2.imwrite('../calibration_wide/test_undist.jpg',dst)
+
+# save the camera calibration result for later use
+dist_pickle = {}
+dist_pickle["mtx"] = mtx
+dist_pickle["dist"] = dist
+
+# save the camera calibration for later use 
+pickle.dump(dist_pickle, open("../calibration_wide/wide_dist_pickle.p", "wb"))
+
+cv2.imshow("Original", img);
+cv2.imshow("Undistroted", dst)
+cv2.waitKey(0)
